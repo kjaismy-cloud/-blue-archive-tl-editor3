20f8982f66fa6b1c78776b6619f13a5aaaf12b1a
kjaismy-cloud <kjaismy@gmail.com>
2026-08-14 14:17:11 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='ba-tl-v4-5-1';
+const CACHE='ba-tl-v4-5-2';
 const SHELL=["./", "./index.html", "./students_local.json", "./raids_local.json", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png"];
 
 self.addEventListener('install',event=>{
